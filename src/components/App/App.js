d2c453d1740e15aeb3bda24187ee9b25d0ca8544
Александр Чернилевский <aleksanderterar@gmail.com>
2021-05-22 23:51:40 +0300

    three

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,10 @@
 import React, { useState } from 'react';
-import Modal from '../Modal/Modal.js';
+// import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
-// import InputItem from '../InputItem/InputItem.js';
 import styles from './App.module.css';
-import ItemList from '../ItemList/ItemList.js'
+import Todo from '../Todo/Todo.js';
 
 const App = () => {
-  const [modalActive, setModalActive] = useState(false);
   return (
     <div className={styles.wrap}>
       <header className={styles.header}>
@@ -14,15 +12,9 @@ const App = () => {
             Приложение
         </div>
       </header>
-      <ItemList />
-      <Button variant='outlined' color='primary' onClick={() => setModalActive(true)}>
-      Добавить Запрос
-      </Button>
-      <Button variant="outlined" color="secondary">
-        Тест GraphQL
-      </Button>
-      <Modal active={modalActive} setActive={setModalActive}>
-      </Modal>
+      <div className={styles.content}>
+        <Todo />
+      </div>
     </div>  
   )
 };
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import styles from './Modal.css';
 import classnames from 'classnames';
-// import Button from '@material-ui/core/Button';
-// import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
 import InputItem from '../InputItem/InputItem.js';
 
